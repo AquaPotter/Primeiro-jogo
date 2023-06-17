78d97bafc05269f28ba002c8fc8a0a3703b374f6
JoanaFarias24 <joanasantanasilva@outlook.com>
2023-06-17 11:57:54 -0300

    Criar movimento.cs

---
+++ Assets/scripts/movimento.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+    //variável da velocidade do personagem
+    public int velocidade = 5;
+
+    //variáveis dos "inputs" (controles/botões) que nosso jogador está apertando
+    float inputVertical;
+    float inputHorizontal;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Detecta os controles e assimila eles às variáveis
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical   = Input.GetAxis("Vertical");
+
+        //Aqui move o personagem
+        transform.Translate(Vector3.forward * Time.deltaTime * velocidade * inputVertical);
+        transform.Translate(Vector3.right   * Time.deltaTime * velocidade * inputHorizontal);
+
+    }
+}
